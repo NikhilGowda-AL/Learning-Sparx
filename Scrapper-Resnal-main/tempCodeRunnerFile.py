def9c1a347d1ad8a380ab22ce47b0442ed7cac75
Nikhil AL <nikhilal.2703@gmail.com>
2024-08-19 19:31:33 +0530

    Delete Scrapper-Resnal-main directory

--- Scrapper-Resnal-main/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-headers["Cookie"] = response.headers["Set-Cookie"].split(";")[0]
-# response = requests.get(img_url, headers=post_headers, stream=True, verify=False)
-# path = Path(__file__).resolve().parent / "cap.jpeg"
-# with open(path, 'wb') as f:
-#     for chunk in response.iter_content(chunk_size=128):
-#         f.write(chunk)
